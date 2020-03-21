a392e21e87e41775405a6389c2f7ca5e8beaed40
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2020-03-21 16:09:43 +0100

    saved dataset into np array

--- scale_images.py
+++ scale_images.py
@@ -1,31 +1,39 @@
 import cv2
 import os
+import numpy as np
+import glob
 
+
+BASE_PATH = 'GTSRB/Final_Training/Images'
 
 def resize():
-    base_path = 'GTSRB/Final_Training/Images'
     counter = 0
-    for dirname in os.listdir(base_path):
-        for filename in os.listdir(os.path.join(base_path, dirname)):
+    for dirname in os.listdir(BASE_PATH):
+        for filename in os.listdir(os.path.join(BASE_PATH, dirname)):
             if filename.endswith(".ppm"):
-                image_path = os.path.join(base_path, dirname, filename)
+                image_path = os.path.join(BASE_PATH, dirname, filename)
                 im = cv2.imread(image_path)
                 resized_im = cv2.resize(im, (32, 32))
                 cv2.imwrite(image_path, resized_im)
 
 
 def sanity_check():
-    base_path = 'GTSRB/Final_Training/Images'
     counter = 0
-    for dirname in os.listdir(base_path):
-        for filename in os.listdir(os.path.join(base_path, dirname)):
+    for dirname in os.listdir(BASE_PATH):
+        for filename in os.listdir(os.path.join(BASE_PATH, dirname)):
             if filename.endswith(".ppm"):
-                image_path = os.path.join(base_path, dirname, filename)
+                image_path = os.path.join(BASE_PATH, dirname, filename)
                 im = cv2.imread(image_path)
                 if im.shape != (32, 32, 3):
                     counter += 1
     print(counter)
 
 
-# resize()
-# sanity_check()
+def load_dataset():
+    filelist = glob.glob(f'{BASE_PATH}/*/*.ppm')
+    X_train = np.array([np.array(cv2.imread(fname)) for fname in filelist])
+    print(X_train.shape)
+
+
+
+load_dataset()
