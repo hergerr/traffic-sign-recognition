3e04fc0c3656f77da55b7c68bc6c2372f02ab1e8
Werdon4 <filipgajewski4@gmail.com>
2020-03-22 10:16:16 +0100

    Label test set
    Function load_dataset returns tuple of 4 elements
    X_train, Y_train, X_test and Y_test
    To work properly in project in folder GTSRB
    must be dir Final_Test with dir Images
    and file GT-final_test.csv from GTSRB site

--- scale_images.py
+++ scale_images.py
@@ -2,9 +2,12 @@ import cv2
 import os
 import numpy as np
 import glob
+import csv
+from matplotlib import pyplot as plt
+import random
 
 BASE_PATH = 'GTSRB/Final_Training/Images'
-TEST_PATH = 'GTSRB/Final_Test/Images'
+TEST_PATH = 'GTSRB/Final_Test'
 
 def resize():
     counter = 0
@@ -31,18 +34,35 @@ def sanity_check():
 
 def load_dataset():
     filelist = glob.glob(f'{BASE_PATH}/*/*.ppm')
-    testlist = glob.glob(f'{TEST_PATH}/*.ppm')
+    testlist = glob.glob(f'{TEST_PATH}/Images/*.ppm')
     X_train = np.array( [np.array( cv2.imread(fname) ) for fname in filelist] )
-    #X_test = np.array([np.array( cv2.imread(fname) ) for fname in testlist])
+    X_test = np.array([np.array( cv2.imread(fname) ) for fname in testlist])
 
     Y_train = np.array([np.zeros(43) for fname in filelist])
     for i,fname in enumerate(filelist):
         Y_train[i][int(fname.split('/')[3])]=1
 
-    return X_train, Y_train
+    with open(f'{TEST_PATH}/GT-final_test.csv') as csvfile:
+        spamreader = csv.reader(csvfile,delimiter=';')
+        data = list(spamreader)
+        data.pop(0)
 
+    Y_test = np.array([np.zeros(43) for fname in testlist])
+    for i,fname in enumerate(testlist):
+        image_name = fname.split('/')[3]
+        image_number = int(image_name.split('.')[0])
+        classid = int(data[image_number][7])
+        Y_test[i][classid]=1
+
+    return X_train, Y_train, X_test, Y_test
+
+def show_image(index, X, Y):
+    plt.imshow(cv2.cvtColor(X[index],cv2.COLOR_BGR2RGB))
+    plt.show()
+    print('Belongs to class nr: ' + str(int(np.where(Y[index]==1)[0])))
 
 
 #resize()
-sanity_check()
-load_dataset()
+#sanity_check()
+#dataset=load_dataset()
+#show_image(random.randint(0,12629),dataset[0], dataset[1])
